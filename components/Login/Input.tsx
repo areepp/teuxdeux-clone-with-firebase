122f17c08dda71fcfa4546b9eb858d450291d57c
param pam pam <arifamjy@gmail.com>
2022-11-09 16:49:53 +0700

    chore: use react-hook-form

--- components/Login/Input.tsx
+++ components/Login/Input.tsx
@@ -1,10 +1,23 @@
-const Input = ({ text }: { text: string }) => {
+import { Inputs } from '@/pages/login'
+import { UseFormRegister } from 'react-hook-form'
+
+interface Props {
+  text: 'email' | 'password'
+  register: UseFormRegister<Inputs>
+}
+
+const Input = ({ text, register }: Props) => {
   return (
     <div className="w-full border border-gray-200 rounded flex flex-col px-4 py-2">
       <label htmlFor={`${text}-input`} className="text-xs">
         {text}
       </label>
-      <input id={`${text}-input`} type="text" className="focus:outline-none" />
+      <input
+        id={`${text}-input`}
+        type="text"
+        className="focus:outline-none"
+        {...register(text, { required: true })}
+      />
     </div>
   )
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,16 +1,37 @@
 import Input from '@/components/Login/Input'
 import Link from 'next/link'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="w-full px-4">
         <h1 className="font-display text-5xl tracking-tight text-center">
           SIGN UP
         </h1>
-        <form className="mt-16 space-y-4">
-          <Input text="email" />
-          <Input text="password" />
+        <form className="mt-16 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <Input text="email" register={register} />
+          {errors.email && (
+            <span className="text-xs">This field is required</span>
+          )}
+
+          <Input text="password" register={register} />
+          {errors.password && (
+            <span className="text-xs">This field is required</span>
+          )}
+
           <button
             className="w-full text-lg py-4 bg-red-600 rounded text-gray-100"
             type="submit"
