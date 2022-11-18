00e9faf33dcaedda7482450bff417a7c5df77745
param pam pam <arifamjy@gmail.com>
2022-11-18 10:42:54 +0700

    style: fix items height

--- components/Dashboard/Todos.tsx
+++ components/Dashboard/Todos.tsx
@@ -1,13 +1,18 @@
 import { getTodo } from '@/lib/todoService'
-import TodoItem from './TodoItem'
 
 const Todos = () => {
   const { todos } = getTodo()
   return (
     <div className="mt-12 h-full flex-grow bg-horizontal-lines">
-      {todos.map((todo) => (
-        <TodoItem data={todo} key={todo.id} />
+      {todos.map((item) => (
+        <div className="h-[49px] flex items-center" key={item.id}>
+          {item.todo}
+        </div>
       ))}
+      <input
+        className="h-[49px] flex items-center w-full focus:outline-none bg-transparent"
+        type="text"
+      />
     </div>
   )
 }
