f718df48410c11c28fab14073327c912d0b00a5b
param pam pam <arifamjy@gmail.com>
2022-12-15 12:09:03 +0700

    style: header logout button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,31 @@
+import * as authService from '@/lib/auth.service'
+import { useRouter } from 'next/router'
+
+const SignOutBtn = () => {
+  const router = useRouter()
+  const handleLogOut = async () => {
+    await authService.logOut()
+    console.log('hei')
+    router.push('/login')
+  }
+
+  return (
+    <div className="absolute h-11 top-0 right-4 flex items-center">
+      <button
+        className="bg-white px-2 py-1 rounded text-xs font-inter text-primary"
+        onClick={handleLogOut}
+      >
+        log out
+      </button>
+    </div>
+  )
+}
+
 const Header = () => {
   return (
-    <header className="flex-none w-full h-11 bg-primary font-gothic text-xl text-gray-200 flex items-center justify-center">
-      BLUPBLUPBLUP
+    <header className="relative flex-none w-full h-11 bg-primary font-gothic text-xl text-gray-200 flex items-center justify-center">
+      <h1>BLUPBLUPBLUP</h1>
+      <SignOutBtn />
     </header>
   )
 }
